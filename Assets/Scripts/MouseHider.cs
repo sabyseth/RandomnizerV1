9abb68d20ee342dbafd77346ed24e47f8dac8fc8
Rafferty <rcruikshank2744@student.pps.net>
2024-10-28 11:20:01 -0700

    main menu

---
+++ Assets/Scripts/MouseHider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+
+}
