135d8ec1a59e76f7eb52779fa66ca316adfa6a65
Rafferty <rcruikshank2744@student.pps.net>
2024-11-22 11:40:20 -0800

    gun recoil

--- Assets/GunDamage.cs
+++ Assets/GunDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GunDamage : MonoBehaviour
 {
     public float Damage;
+    public Recoil RecoilObject;
     public float BulletRange;
     public ParticleSystem MuzzleFlash;
     private Transform PlayerCamera;
@@ -21,15 +22,13 @@ public class GunDamage : MonoBehaviour
 
     public void Shoot()
     {
-        
-
        Ray gunRay = new Ray(bulletSpawnPoint.position, transform.forward);
         if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
         {
         MuzzleFlash.Play();
+        RecoilObject.recoil += 0.005f;
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-        
             if (hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
             {
                 enemy.Health -= Damage;
