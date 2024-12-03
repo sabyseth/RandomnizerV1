6e0d76340de27569f8143d3d8ac425cf246a0893
Rafferty <rcruikshank2744@student.pps.net>
2024-12-03 11:40:34 -0800

    Bullet trail

--- Assets/GunDamage.cs
+++ Assets/GunDamage.cs
@@ -12,6 +12,7 @@ public class GunDamage : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    public TrailRenderer BulletTrail;
 
     private void Start()
     {
@@ -27,13 +28,30 @@ public class GunDamage : MonoBehaviour
         {
         MuzzleFlash.Play();
         RecoilObject.recoil += 0.005f;
-        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        //var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        //bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        TrailRenderer trail = Instantiate(BulletTrail, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        StartCoroutine(SpawnTrail(trail, hitInfo));
             if (hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
             {
                 enemy.Health -= Damage;
                 Debug.Log("hit");
             }
         }
+    }
+    private IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit hit)
+    {
+        float distance = Vector3.Distance(Trail.transform.position, hit.point);
+        float startingDistance = distance;
+        Vector3 startposition = Trail.transform.position;
+
+        while (distance > 0)
+        {
+            Trail.transform.position = Vector3.Lerp(startposition,hit.point, 1 - (distance / startingDistance));
+            distance -= Time.deltaTime * bulletSpeed;
+
+           yield return null;
+        }
+        Trail.transform.position = hit.point;
     }
 }
