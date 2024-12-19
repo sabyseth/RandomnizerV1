9e8a0645ae7078734f5ffeb7ad90754232f5dc5f
Dom <dleporepdx@gmail.com>
2024-12-19 12:04:04 -0800

    nothing useful

--- Assets/_project/Runtime/PlayerCamera.cs
+++ Assets/_project/Runtime/PlayerCamera.cs
@@ -9,6 +9,7 @@ public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] private float sensitivity = 0.1f;
     private Vector3 _eulerAngles;
+    public float maxPitch = 80f;
 
     public void Initialize(Transform target)
     {
@@ -20,7 +21,7 @@ public class PlayerCamera : MonoBehaviour
     public void UpdateRotation(CameraInput input)
     {
         _eulerAngles += new Vector3(-input.Look.y, input.Look.x) * sensitivity;
-        transform.eulerAngles = _eulerAngles;
+        transform.eulerAngles = Vector3.ClampMagnitude(_eulerAngles, 100f);
     }
 
     public void UpdatePosition(Transform target)
