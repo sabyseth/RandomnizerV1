e902dbf651d4edf6392a132d5e8e4b243c112146
Rafferty <rcruikshank2744@student.pps.net>
2025-01-14 11:41:00 -0800

    main menu

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+public class MenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _button;
+
+    private void Awake(){
+    _document = GetComponent<UIDocument>();
+
+    _button = _document.rootVisualElement.Q("StartGameButton") as Button;
+    _button.RegisterCallback<ClickEvent>(OnplayGameClick);
+    }
+
+    private void OnDisable(){
+        _button.UnregisterCallback<ClickEvent>(OnplayGameClick);
+    }
+
+    private void OnplayGameClick(ClickEvent evt){
+        Debug.Log("Start");
+    }
+}
