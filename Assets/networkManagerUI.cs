c4f7737e9bef9a70aec024b15fdf3aa3d4eb9418
Dom <dleporepdx@gmail.com>
2025-02-19 12:05:25 -0800

    More UI Button stuff

--- Assets/networkManagerUI.cs
+++ Assets/networkManagerUI.cs
@@ -8,30 +8,37 @@ using UnityEngine.UIElements;
 public class networkManagerUI : MonoBehaviour
 {
     private UIDocument _document;
-    private Button _button;
+    private Button _server;
+    // public struct Buttons
+    // {
+    //     Button _server;
+    //     Button _host;
+    // }
     private List<Button> _serverUIButtons = new List<Button>();
 
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
         
-        _button = _document.rootVisualElement.Q("Server") as Button;
-        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
-        // _button = _document.rootVisualElement.Q("Host") as Button;
-        // _button = _document.rootVisualElement.Q("Client") as Button;
+        // Buttons._server = _document.rootVisualElement.Q("Server") as Button;
+        // _server.RegisterCallback<ClickEvent>(onServerClick);
+        // _server = _document.rootVisualElement.Q("Host") as Button;
+        // _server.RegisterCallback<ClickEvent>(onServerClick);
+        _server = _document.rootVisualElement.Q("Client") as Button;
+        _server.RegisterCallback<ClickEvent>(onServerClick);
 
-        _serverUIButtons = _document.rootVisualElement.Query<Button>().ToList();
-        for (int i = 0; i < _serverUIButtons.Count; i++)
-        {
-            _serverUIButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
-        }
+        // _serverUIButtons = _document.rootVisualElement.Query<Button>().ToList();
+        // for (int i = 0; i < _serverUIButtons.Count; i++)
+        // {
+        //     _serverUIButtons[i].RegisterCallback<ClickEvent>(OnAllButtonsClick);
+        //     if(i == 0) { }
+        // }
 
         
     }
-
     private void OnDisable()
     {
-        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _server.UnregisterCallback<ClickEvent>(onServerClick);
 
         for (int i = 0; i < _serverUIButtons.Count; i++)
         {
@@ -39,9 +46,9 @@ public class networkManagerUI : MonoBehaviour
         }
     }
 
-    private void OnPlayGameClick(ClickEvent evt)
+    private void onServerClick(ClickEvent evt)
     {
-        Debug.Log("Server BTN Pressed");
+        Debug.Log("server btn pressed");
     }
 
     private void OnAllButtonsClick(ClickEvent evt)
